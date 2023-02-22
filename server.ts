842f6adbc52b4ea63923090a5f356352e4648248
Reed von Redwitz <reed+github@vonredwitz.com>
2023-02-22 09:05:21 +0100

    small change to test render redeploy

--- server.ts
+++ server.ts
@@ -2,6 +2,6 @@ import { serve } from "https://deno.land/std@0.173.0/http/server.ts";
 
 serve(() => {
   return new Response(
-    `Hello from deno.`,
+    `Hello from deno!`,
   );
 }, { port: 8080 });
